1ab4cedb2c8cd7d432330485bcb5e3ec93e94b83
unknown <manalferr@gmail.com>
2024-07-01 11:52:12 +0300

    Task sission 16

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,24 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return 'Welcome to the admin route!';
 });
+
+
+// Define the admin route
+Route::get('/admin', [AdminController::class, 'index'])->name('index');
+
+// Define register routes as needed
+
+
+Route::get('/auth/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+
+
+
+
+
+
